0c7e734a85b7e91a5e3a7c5b644b5eb46043667c
cdubos-fr <contact@cdubos.fr>
2024-11-11 10:42:42 +0000

    fix(test): skip GUI test

--- tests/test_app.py
+++ tests/test_app.py
@@ -6,12 +6,12 @@ from pdf_img_converter_gui.app import MyApp
 from pdf_img_converter_gui.app import MyWidget
 
 
+@pytest.mark.skip(reason='Current CI does not support GUI testing')
 class TestApp(GraphicUnitTest):
     def setUp(self) -> None:
         Builder.load_file('pdf_img_converter_gui/myapp.kv')
         return super().setUp()
 
-    @pytest.mark.skip(reason='Current CI does not support GUI testing')
     def test_app_rendering(self) -> None:
         app = MyApp()
 
@@ -22,13 +22,11 @@ class TestApp(GraphicUnitTest):
             == widget.ids.conversion_mode.children[0]
         )
 
-    @pytest.mark.skip(reason='Current CI does not support GUI testing')
     def test_get_widget(self) -> None:
         app = MyApp()
 
         assert isinstance(app.build(), MyWidget)
 
-    @pytest.mark.skip(reason='Current CI does not support GUI testing')
     def test_selected_files_is_empty(self) -> None:
         app = MyApp()
 
