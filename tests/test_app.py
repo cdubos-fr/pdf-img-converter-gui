806857ae185542b4f68fa75a853cd674cf48b398
cdubos-fr <contact@cdubos.fr>
2024-11-11 10:49:26 +0000

    fix(test): GUI import in test

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,33 +1,30 @@
 import pytest
-from kivy.lang import Builder
 from kivy.tests.common import GraphicUnitTest
-
-from pdf_img_converter_gui.app import MyApp
-from pdf_img_converter_gui.app import MyWidget
 
 
 @pytest.mark.skip(reason='Current CI does not support GUI testing')
 class TestApp(GraphicUnitTest):
     def setUp(self) -> None:
+        from kivy.lang import Builder
+
+        from pdf_img_converter_gui.app import MyApp
+
         Builder.load_file('pdf_img_converter_gui/myapp.kv')
+        self.app = MyApp()
         return super().setUp()
 
     def test_app_rendering(self) -> None:
-        app = MyApp()
-
-        app.run()
-        widget = app.root
+        self.app.run()
+        widget = self.app.root
         assert (
             widget.ids.conversion_mode.current_active_segment
             == widget.ids.conversion_mode.children[0]
         )
 
     def test_get_widget(self) -> None:
-        app = MyApp()
+        from pdf_img_converter_gui.app import MyWidget
 
-        assert isinstance(app.build(), MyWidget)
+        assert isinstance(self.app.build(), MyWidget)
 
     def test_selected_files_is_empty(self) -> None:
-        app = MyApp()
-
-        assert app.selected_files == []
+        assert self.app.selected_files == []
